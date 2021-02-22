4b155bba93d31ddc35b1a3a7f4ee32f8f7b6354c
Chas Johson <chasjohnson09@gmail.com>
2021-02-22 12:23:59 -0500

    12:23 push

---
+++ LibraryTutorial/LibraryTutorial/MathLib.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace LibraryTutorial
+{
+    public class MathLib
+    {
+        public string About { get; set; } = "About MathLib";
+    }
+}
---
+++ LibraryTutorial/TestMathLib/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using LibraryTutorial;
+
+namespace TestMathLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mathlib = new MathLib();
+            Console.WriteLine($"About is {mathlib.About}");
+        }
+    }
+}
