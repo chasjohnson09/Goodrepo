071c0d8a3feacccbdcfa67d85df2e4155d2b5cbf
Chas Johson <chasjohnson09@gmail.com>
2021-02-22 10:08:50 -0500

    init commit

---
+++ GenericListPractice/GenericListPractice/Friend.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericListPractice
+{
+    class Friend
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool BFF { get; set; }
+    }
+}
---
+++ GenericListPractice/GenericListPractice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericListPractice
+{
+    class Program       // check class notes under generic collection for what the instructions were
+    {
+        static void Main(string[] args)
+        {
+            var friends = new List<Friend>();
+
+            var bob = new Friend { Name = "Bob", Email = "Bob123@yahoo.com", BFF = true };
+            friends.Add(bob);
+            var frank = new Friend { Name = "Frank", Email = "frank534@gmail.com", BFF = false };
+            friends.Add(frank);
+            var pam = new Friend { Name = "Pam", Email = "pam83949@yhoo.com", BFF = true };
+            friends.Add(pam);
+
+            foreach(var friend in friends)
+            {
+                Console.WriteLine($"{friend.Name}");
+            }
+            
+        }
+    }
+}
