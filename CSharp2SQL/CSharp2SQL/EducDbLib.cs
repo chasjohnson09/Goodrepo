30f220824dbcbcd38509a65336f9b8d08ed5da2a
Chas Johson <chasjohnson09@gmail.com>
2021-02-22 15:52:01 -0500

    init commit

---
+++ CSharp2SQL/CSharp2SQL/EducDbLib.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+namespace CSharp2SQL
+{
+    public class EducDbLib
+    {
+        public  SqlConnection connection { get; set; }
+        
+
+        public void Connect(string database)
+        {
+            var connStr = $"localhost\\sqlexpress;" +
+                            $"database={database};" +
+                            "trusted_connection=true;";
+            connection = new SqlConnection(connStr);
+            connection.Open();
+        }
+        
+    }
+}
---
+++ CSharp2SQL/TestCSharp2SQL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestCSharp2SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public void Connect(string database)
+            {
+
+            }
+        }
+    }
+}
