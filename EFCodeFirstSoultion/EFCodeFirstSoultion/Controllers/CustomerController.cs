47bc73bd40fbf171e47c9047e8b554041c6588bf
Chas Johson <chasjohnson09@gmail.com>
2021-03-01 21:00:30 -0500

    first homework commit

--- EFCodeFirstSoultion/EFCodeFirstSoultion/Controllers/CustomerController.cs
+++ EFCodeFirstSoultion/EFCodeFirstSoultion/Controllers/CustomerController.cs
@@ -37,6 +37,52 @@ namespace EFCodeFirstSoultion
                 throw new Exception("Customer ID MUST be 0");
             }
             await _context.Customers.AddAsync(customer);
+            var rowsaffected = await _context.SaveChangesAsync();
+            if(rowsaffected != 1)
+            {
+                throw new Exception("Create failed!!!");
+            }
+            return customer;
         }
+
+
+        public async Task Change(Customer customer)
+        {
+            if(customer == null)
+            {
+                throw new Exception("Customer cannot be NULL");
+            }
+            if(customer.Id < 0)
+            {
+                throw new Exception("Customer ID must be greater than ZERO");
+            }
+            _context.Entry(customer).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            var rowsaffected = await _context.SaveChangesAsync();
+            if(rowsaffected != 1)
+            {
+                throw new Exception("Change failed!!");
+            }
+            return;
+        }
+
+
+        public async Task<Customer> Remove(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return null;
+            }
+            _context.Customers.Remove(customer);
+            var rowsAffected = await _context.SaveChangesAsync();
+            if (rowsAffected != 1)
+            {
+                throw new Exception("Remove Failed!!");
+            }
+            return customer;
+        }
+
+
+
     }
 }
