3590c37788068738799a862b72e634b4d0c664d5
Chas Johson <chasjohnson09@gmail.com>
2021-03-08 14:06:34 -0500

    after lunch commit

--- PRS/PrsServer/PrsServer/Data/PrsDbContext.cs
+++ PRS/PrsServer/PrsServer/Data/PrsDbContext.cs
@@ -22,6 +22,24 @@ namespace PrsServer.Data
             {
                 e.HasIndex(u => u.Username).IsUnique(true);
             });
+
+            builder.Entity<Vendor>(e =>
+            {
+                e.HasIndex(v => v.Code).IsUnique(true);
+            });
+
+            builder.Entity<Product>(e =>
+            {
+                e.HasIndex(v => v.PartNbr).IsUnique(true);
+            });
         }
+
+        public DbSet<PrsServer.Models.Product> Product { get; set; }
+
+        public DbSet<PrsServer.Models.Vendor> Vendor { get; set; }
+
+        public DbSet<PrsServer.Models.Request> Request { get; set; }
+
+        public DbSet<PrsServer.Models.RequestLine> RequestLine { get; set; }
     }
 }
---
+++ PRS/PrsServer/PrsServer/Models/RequestLine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrsServer.Models
+{
+    public class RequestLine
+    {
+        public RequestLine() { }
+        public int Id { get; set; }
+        [Required]
+        public int RequestId { get; set; }
+        public virtual Request Request { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+        [Required]
+        public int Qunatity { get; set; } = 0;
+    }
+}
