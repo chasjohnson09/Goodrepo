479b73be419ca2cc09b7cbca0c4c37423d8d21af
Chas Johson <chasjohnson09@gmail.com>
2021-03-23 08:52:52 -0400

    hello

---
+++ Remedial-project/Remedial-project/models/Service.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remedial_project.models
+{
+    class Service
+    {
+        public int Id { get; set; }
+        public int ServiceYears { get; set; }
+    }
+}
